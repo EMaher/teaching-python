e44220b21c3c5f24e82e40833c568d4f9bd807ea
Elizabeth Maher <enewman@microsoft.com>
2024-09-21 03:29:05 +0000

    Added images for workshop

--- Ignite/Python-Turtle/create-pdf.sh
+++ Ignite/Python-Turtle/create-pdf.sh
@@ -1,3 +1,3 @@
-#! /bin/bash
+#!/bin/bash
 asciidoctor-pdf PythonTutorial-SquaresAndFlowers-MS.adoc -o PythonTutorial-SquaresAndFlowers-MS.pdf
 asciidoctor-pdf PythonTutorial-WhatsNext.adoc -o PythonTutorial-WhatsNext.pdf
