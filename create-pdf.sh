df5cddde3f9008aaf299ca6868bd1570d5ca8e22
Elizabeth Maher <enewman@microsoft.com>
2024-10-02 06:27:09 +0000

    initial draft of MakeCode-Turtles-Flowers curriculum.

--- create-pdf.sh
+++ create-pdf.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 asciidoctor-pdf Trinket/PythonTutorial-SquaresAndFlowers-MS.adoc -o bin/Trinket/PythonTutorial-SquaresAndFlowers-MS.pdf
 asciidoctor-pdf Trinket/PythonTutorial-WhatsNext.adoc -o bin/Trinket/PythonTutorial-WhatsNext.pdf
+
+asciidoctor-pdf -a allow-uri-read MakeCode/PythonTutorial-SquaresAndFlowers-MS.adoc -o bin/MakeCode/PythonTutorial-SquaresAndFlowers-MS.pdf
