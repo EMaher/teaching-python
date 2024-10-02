4f75f0f1604224c7866d6ef7d93a09dcc7d12bcd
Elizabeth Maher <enewman@microsoft.com>
2024-10-02 01:34:22 +0000

    re-organizing lessons by tool

--- Ignite/Python-Turtle/create-pdf.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-asciidoctor-pdf PythonTutorial-SquaresAndFlowers-MS.adoc -o PythonTutorial-SquaresAndFlowers-MS.pdf
-asciidoctor-pdf PythonTutorial-WhatsNext.adoc -o PythonTutorial-WhatsNext.pdf
---
+++ create-pdf.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+asciidoctor-pdf Trinket/PythonTutorial-SquaresAndFlowers-MS.adoc -o bin/Trinket/PythonTutorial-SquaresAndFlowers-MS.pdf
+asciidoctor-pdf Trinket/PythonTutorial-WhatsNext.adoc -o bin/Trinket/PythonTutorial-WhatsNext.pdf
