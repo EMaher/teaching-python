f6ac16a1327fa79aec7fc65270d30d1d9f0f43be
Elizabeth Maher <enewman@microsoft.com>
2025-05-13 11:27:09 -0700

    saving work

--- create-pdf.sh
+++ create-pdf.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
+echo "Creating PDF files from AsciiDoc files..."
+
+# Check if asciidoctor-pdf is installed
+if ! command -v asciidoctor-pdf &> /dev/null
+then
+    echo "asciidoctor-pdf could not be found. Please install it to proceed."
+    exit
+fi
+
 for file in $(find -type f -name *.adoc); do
-    echo $file
+    echo "Processing $file..."
     if [ -f "$file" ]; then
         asciidoctor-pdf -a allow-uri-read -a icons=font -a icon-set=fa   "$file" -o "bin/${file%.[^.]*}-digital.pdf"
         asciidoctor-pdf -a allow-uri-read -a show-link-uri -a icons=font -a icon-set=fa   "$file" -o "bin/${file%.[^.]*}-print.pdf"
